298c7813163e4b08b845e67ca35129a2472f7459
Joseph Karl Magnussen <jkmagnussen@outlook.com>
2020-07-14 15:42:26 +0100

    App title change

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello worldd</Text>
+      <Text>Hello worlddd</Text>
       <StatusBar style="auto" />
     </View>
   );
