0c1474b2e8998e9d39844c70bd021d4f8c86a3a9
Joseph Karl Magnussen <jkmagnussen@outlook.com>
2020-07-18 00:29:51 +0100

    app edit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello worldddss</Text>
+      <Text>Hello worldddssfs</Text>
       <StatusBar style="auto" />
     </View>
   );
