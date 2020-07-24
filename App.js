70f58f516eeb9e59f168c4b9ebd7bd5a7f8350f8
Joseph Karl Magnussen <jkmagnussen@outlook.com>
2020-07-24 21:01:50 +0100

    updated

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { View } from "react-native";
+import AddEntry from "./components/addEntry";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hellooo worllddds</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <AddEntry />
+      </View>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
