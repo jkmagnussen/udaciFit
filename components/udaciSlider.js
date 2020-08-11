362784999090f728b09e1e82f801e995feb30d80
Joseph Karl Magnussen <jkmagnussen@outlook.com>
2020-08-11 13:46:08 +0100

    a

--- components/udaciSlider.js
+++ components/udaciSlider.js
@@ -13,7 +13,7 @@ export default function udaciSlider({ max, unit, step, value, onChange }) {
         onValueChange={onChange}
       />
 
-      <Text>A{value}</Text>
+      <Text>{value}</Text>
       <Text>{unit}</Text>
     </View>
   );
