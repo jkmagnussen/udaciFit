f85e543e0ea6cd12e7c758291870b40cae55b9d9
Joseph Karl Magnussen <jkmagnussen@outlook.com>
2020-09-02 12:41:22 +0100

    great

--- components/DateHeader.js
+++ components/DateHeader.js
@@ -2,5 +2,5 @@ import React, { Component } from "react";
 import { Text } from "react-native";
 
 export default function DateHeader({ date }) {
-  return <Text>Not Great: {date}</Text>;
+  return <Text>{date}</Text>;
 }
