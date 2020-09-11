5c86e00997d2d5f68b2d5e8fa1eb43d5f201302c
Joseph Karl Magnussen <jkmagnussen@outlook.com>
2020-09-11 10:51:06 +0100

    up

--- components/DateHeader.js
+++ components/DateHeader.js
@@ -2,5 +2,5 @@ import React, { Component } from "react";
 import { Text } from "react-native";
 
 export default function DateHeader({ date }) {
-  return <Text>S:{date}</Text>;
+  return <Text>Hello{date}</Text>;
 }
