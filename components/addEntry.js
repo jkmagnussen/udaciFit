9437b5fb91b7da9f3811b33c491394d442a2190b
Joseph Karl Magnussen <jkmagnussen@outlook.com>
2020-09-19 12:51:23 +0100

    lesson 3

--- components/addEntry.js
+++ components/addEntry.js
@@ -4,6 +4,8 @@ import { getMetricMetaInfo, timeToString } from "../utils/helpers";
 import UdaciSlider from "./udaciSlider";
 import UdaciSteppers from "./udaciSteppers";
 import DateHeader from "./DateHeader";
+import TextButton from "./TextButton";
+import { Ionicons } from "@expo/vector-icons";
 
 function SubmitBtn({ onPress }) {
   return (
@@ -56,7 +58,7 @@ export default class AddEntry extends Component {
     const key = timeToString();
     const entry = this.state;
 
-    // uPDATE rEDUX
+    // Update Redux
 
     this.setState(() => ({
       run: 0,
@@ -73,8 +75,28 @@ export default class AddEntry extends Component {
     //CLEAR LOCAL NOTIFICATION
   };
 
+  reset = () => {
+    const key = timeToString();
+
+    //Update Redux
+
+    //Route to hOME
+
+    //Update DB
+  };
+
   render() {
     const metaInfo = getMetricMetaInfo();
+
+    if (this.props.alreadyLogged) {
+      return (
+        <View>
+          <Ionicons name="ios-happy-outline" size={100} />
+          <Text>You already logged your information for today</Text>
+          <TextButton onPress={this.reset}>Reset</TextButton>
+        </View>
+      );
+    }
 
     return (
       <View>
