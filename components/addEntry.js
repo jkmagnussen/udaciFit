f75e34f22302b0b6e1753c06f83f560a97ff34c4
Joseph Karl Magnussen <jkmagnussen@outlook.com>
2020-11-10 00:51:23 +0000

    br

--- components/addEntry.js
+++ components/addEntry.js
@@ -74,7 +74,7 @@ export default class AddEntry extends Component {
 
     //Clear Local Navigation
 
-    // Machette, bad times at the el royale, Star Trek into darkness, Predator, Human Centipede, Thor Ragnarok, Vanhelsing, Eagle vs Shark, Underworld, The Blare Witch Project, Spider-Man 1 & 2, Terminator Dark Fate, Polar, The Weary Kind
+    // Machette, bad times at the el royale, Star Trek into darkness, Predator, Human Centipede, Thor Ragnarok, Vanhelsing, Eagle vs Shark, Underworld, The
   };
 
   reset = () => {
